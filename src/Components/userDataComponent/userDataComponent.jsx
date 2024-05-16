7ec898ec35ba8020774383c06b10a05a312ea02d
Sudhar2303 <Sudharsananuma23@gmail.com>
2024-05-16 15:38:32 +0530

    first commit

---
+++ src/Components/userDataComponent/userDataComponent.jsx
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+const UserDataComponent = () => {
+  const [userData, setUserData] = useState({});
+
+  // Send the token to db to get details
+  useEffect(()=>{
+    axios
+    .post(`http://localhost:3500/api/v1/userdata`,
+    {
+      token:window.localStorage.getItem('token')
+    })
+  .then(response=>setUserData(response.data))
+  .catch((error)=>{
+    alert(`Status :${error.response.status} - ${error.response.data.message}`)
+  })
+
+  })
+
+  return (
+    <React.Fragment>
+      <h1>User Details</h1>
+      <div className='mb-3'>
+        <p>First Name :</p>
+        <h4>{userData.firstName}</h4>
+        <p>Last Name: </p>
+        <h4>{userData.lastName}</h4>
+        <p>Email: </p>
+        <h4>{userData.email}</h4>
+     
+      </div>
+      
+    </React.Fragment>
+  );
+};
+
+export default UserDataComponent;
